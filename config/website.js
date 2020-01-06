e543d55418fa0c1115827b256412b4782cf9aeb3
arthursoares <arthur@envisioning.io>
2020-01-06 23:48:34 +0100

    Update website.js

--- config/website.js
+++ config/website.js
@@ -2,8 +2,8 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
   title: 'Arthur Soares', // Navigation and Site Title
   titleAlt: 'Arthur Soares', // Title for JSONLD
-  description: 'A typography-heavy & light-themed Gatsby Starter which uses the Headless CMS Prismic.',
-  headline: 'Writing and publishing content for LekoArts', // Headline for schema.org JSONLD
+  description: 'Arthur Soares is a Berlin based Brazilian.',
+  headline: 'Arthur Soares is a Berlin based Brazilian.', // Headline for schema.org JSONLD
   url: 'https://arthursoares.com.br', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   logo: '/logos/logo-1024.png', // Used for SEO
